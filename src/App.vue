8826f08aeb81d9cbd4c2b79bd9767cb5120cbb97
hike0701 <hiking74013@gmail.com>
2024-02-13 00:41:27 +0800

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <button type="button" class="btn btn-primary">Primary</button>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -21,8 +20,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style lang="scss">
-$primary:red;
-div{
-  color: $primary;
-}
+@import "./assets/all.scss"
 </style>
