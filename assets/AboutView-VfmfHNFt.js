1f1a9b7396ed14517a4090031c26fdf6670ccf16
InnocentMeow <125479298+InnocentMeow@users.noreply.github.com>
2024-02-12 22:53:54 +0800

    deploy

---
+++ assets/AboutView-VfmfHNFt.js
@@ -0,0 +1,1 @@
+import{c as t,a,t as s,u as o,b as c,o as n}from"./index-BO1DBTLz.js";const r={class:"about"},_=c("h1",null,"This is an about page",-1),d={__name:"AboutView",setup(i){const e="產品文字";return(u,l)=>(n(),t("div",r,[_,a(" "+s(o(e)),1)]))}};export{d as default};
