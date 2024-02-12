92efb4dc40627eaa285bbf5b8efd3bf3b49a3a7f
InnocentMeow <losing74013@gmail.com>
2024-02-12 22:05:35 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ text }}
+  </div>
+</template>
+
+<script setup>
+  const text = import.meta.env.VITE_TEXT;
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
