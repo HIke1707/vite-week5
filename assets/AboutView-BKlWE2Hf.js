dd84f54489507681d55be8ff30bcb027eede61b6
hike0701 <hiking74013@gmail.com>
2024-03-05 23:40:38 +0800

    deploy

--- assets/AboutView-BKlWE2Hf.js
+++
@@ -1,1 +0,0 @@
-import{c as t,d as a,t as s,x as o,a as c,o as n}from"./index-DszBi2mV.js";const r={class:"about"},_=c("h1",null,"This is an about page",-1),d={__name:"AboutView",setup(i){const e="產品文字";return(u,l)=>(n(),t("div",r,[_,a(" "+s(o(e)),1)]))}};export{d as default};
---
+++ assets/FrontProductsList-i2Fkt0Nr.js
@@ -0,0 +1,1 @@
+import{e as t,o as e,_ as o}from"./index-fkq15v92.js";const c={__name:"FrontProductsList",setup(r){return(s,_)=>(e(),t(o))}};export{c as default};
